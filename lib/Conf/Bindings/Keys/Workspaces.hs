b4dce7d937a66197aed5b6f6c3ce23b029ffa377
Maddison Hellstrom <maddy@thundr.net>
2017-08-09 23:16:57 -0700

    M-[1..9] to select workspaces on current screen

--- lib/Conf/Bindings/Keys/Workspaces.hs
+++ lib/Conf/Bindings/Keys/Workspaces.hs
@@ -16,38 +16,66 @@ module Conf.Bindings.Keys.Workspaces
   ( workspaces
   ) where
 
-import Conf.Bindings.Keys.Internal (subKeys, zipM, wsKeys) --, modSym)
+import Data.List
+        ( nub
+        , sortBy
+        , find
+        )
 
-import Conf.Theme (warmPrompt)
+import Conf.Bindings.Keys.Internal
+        ( subKeys
+        , zipM
+        , wsKeys
+        )
+
+import Conf.Theme
+        ( warmPrompt
+        )
 
 import qualified XMonad
+import qualified XMonad.Core as Core
 import qualified XMonad.StackSet as StackSet
 import qualified XMonad.Prompt as Prompt
 
 import qualified XMonad.Actions.CopyWindow as CopyWindow
 import qualified XMonad.Actions.CycleWS as CycleWS
-import qualified XMonad.Actions.DynamicProjects as DynamicProjects
+
+import qualified XMonad.Layout.IndependentScreens as IndependentScreens
+
+import qualified XMonad.Hooks.WorkspaceHistory as WH
 
 import qualified XMonad.Util.NamedScratchpad as NamedScratchpad
 import qualified XMonad.Util.WorkspaceCompare as WorkspaceCompare
 
-import XMonad.Actions.DynamicWorkspaces (withNthWorkspace)
-import XMonad.Util.NamedActions (addName)
+import XMonad.Actions.DynamicWorkspaces
+        ( withNthWorkspace
+        )
+
+import XMonad.Layout.IndependentScreens
+        ( VirtualWorkspace
+        )
+
+import XMonad.Util.NamedActions
+        ( addName
+        )
 
 workspaces c = subKeys "Workspaces & Projects" c
-  ( [ ( "M-w",   addName "Switch to Project"        switchProj)
-    , ( "M-S-w", addName "Shift to Project"         shiftProj)
-    , ( "M-`",   addName "Next non-empty workspace" nextNonEmptyWS)
+  ( [ ( "M-`",   addName "Next non-empty workspace" nextNonEmptyWS)
     , ( "M-S-`", addName "Prev non-empty workspace" prevNonEmptyWS)
     , ( "M-a",   addName "Toggle last workspace"    toggleLast)
-  ]
-  ++ zipM "M-"     "View      ws" wsKeys [0 ..] viewWs
-  ++ zipM "C-"     "Move w to ws" wsKeys [0 ..] moveWinWs
-  ++ zipM "M-S-C-" "Copy w to ws" wsKeys [0 ..] copyWinWs
+    ]
+  ++ [ ("M-" ++ show i, addName "View ws" $ XMonad.windows $ IndependentScreens.onCurrentScreen StackSet.view w)
+     | (i, w) <- zip [0..9] (workspaces' c) ]
+  ++ [ ("C-" ++ show i, addName "Move ws" $ XMonad.windows $ IndependentScreens.onCurrentScreen StackSet.shift w)
+     | (i, w) <- zip [0..9] (workspaces' c) ]
   )
 
-switchProj = DynamicProjects.switchProjectPrompt  warmPrompt
-shiftProj  = DynamicProjects.shiftToProjectPrompt warmPrompt
+workspaces' :: XMonad.XConfig l -> [VirtualWorkspace]
+workspaces' = nub . sortWS . map IndependentScreens.unmarshallW . filterWS . XMonad.workspaces
+
+filterWS = filter (/="0_NSP")
+
+sortWS = sortBy (\a b -> compare a b)
 
 nextNonEmptyWS =
   CycleWS.findWorkspace getSortByIndexNoSP Prompt.Next CycleWS.HiddenNonEmptyWS 1 >>= \t ->
@@ -60,8 +88,17 @@ prevNonEmptyWS =
 getSortByIndexNoSP =
   fmap (. NamedScratchpad.namedScratchpadFilterOutWorkspace) WorkspaceCompare.getSortByIndex
 
-toggleLast = CycleWS.toggleWS' ["0_NSP"] -- Ignore NSP (Named Scratchpad)
+-- toggleLast = CycleWS.toggleWS' ["0_NSP"] -- Ignore NSP (Named Scratchpad)
+toggleLast
+  = do
+    lastViewedHidden --workspaces'
+    return ()
 
-viewWs    = withNthWorkspace StackSet.view
-moveWinWs = withNthWorkspace StackSet.shift
-copyWinWs = withNthWorkspace CopyWindow.copy
+lastViewedHidden :: XMonad.X (Maybe XMonad.WorkspaceId)
+lastViewedHidden = do
+    hs <- XMonad.gets $ map StackSet.tag . StackSet.hidden . Core.windowset
+    vs <- WH.workspaceHistory
+    return $ choose hs (find (`elem` hs) vs)
+    where choose []    _           = Nothing
+          choose (h:_) Nothing     = Just h
+          choose _     vh@(Just _) = vh
