814c3078271b4e39aec921a877004ba9dcf34bc1
Maddison Hellstrom <maddy@thundr.net>
2017-08-14 08:10:57 -0700

    Update fonts

--- lib/Conf/Theme/Fonts.hs
+++ lib/Conf/Theme/Fonts.hs
@@ -17,11 +17,21 @@ module Conf.Theme.Fonts
   , wideFont
   ) where
 
+getFont :: String -> Int -> String
+getFont f s = f ++ ":size=" ++ (show s)
+
+getFontPx :: String -> Int -> String
+getFontPx f s = f ++ ":pixelsize=" ++ (show s)
+
+withStyle :: String -> String -> String
+withStyle f s = f ++ ":style=" ++ s
+
 dejaVu   = "xft:DejaVu Sans Mono for Powerline:hinting=true"
 euroStar = "xft:Eurostar Black Extended:hinting=true"
 
-font      = dejaVu   ++ ":size=10"
-titleFont = dejaVu   ++ ":size=8"
+font      = getFont dejaVu 8 --dejaVu   ++ ":size=10"
+titleFont = getFont dejaVu 8 -- dejaVu   ++ ":size=8"
 -- tabFont   = dejaVu   ++ ":style=Bold:size=8"
-bigFont   = dejaVu   ++ ":size=14"
-wideFont  = euroStar ++ ":style=Regular:pixelsize=180"
+bigFont   = getFont dejaVu 14 --dejaVu   ++ ":size=14"
+-- wideFont  = euroStar ++ ":style=Regular:pixelsize=180"
+wideFont  = getFontPx (withStyle euroStar "Regular") 180
