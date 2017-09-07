9ffb8f0042eb49cddf8d73645e01dfc873a65d8e
Maddison Hellstrom <maddy@thundr.net>
2017-09-07 00:38:10 -0700

    Add script to delete xmonad.state
    Can be useful when starting XMonad on a new display configuration (e.g.
    going from 3 displays to 1 display) to prevent odd behavior from
    leftover state.

---
+++ reset.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+BASEDIR="$(dirname $0)"
+CACHEFILE="$BASEDIR/xmonad.state"
+if [ -f $CACHEFILE ]; then
+	echo "Deleting $CACHEFILE..."
+	rm $CACHEFILE
+else
+	echo "No cache file to delete."
+fi
+
+exit 0
