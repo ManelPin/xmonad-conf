82a9ea4ebf91c4e235da0db31981f99424402a0b
Maddison Hellstrom <maddy@thundr.net>
2017-09-27 23:24:52 -0700

    Center popup windows

--- lib/Conf/Hooks/Manage.hs
+++ lib/Conf/Hooks/Manage.hs
@@ -56,7 +56,7 @@ manageSpecific =
     , resource =? "console" -?> tileBelowNoFocus
     , isRole =? gtkFile -?> forceCenterFloat
     -- , className =? Apps.passwordMgrClass -?> doIgnore
-    -- , isRole =? "pop-up" -?> doCenterFloat
+    , isRole =? "pop-up" -?> doCenterFloat
     , isDialog -?> doCenterFloat
     , isInProperty "_NET_WM_WINDOW_TYPE" "_NET_WM_WINDOW_TYPE_SPLASH" -?> doCenterFloat -- TODO: Cleanup into 'isSplashScreen'
     , isFullscreen -?> doFullFloat
