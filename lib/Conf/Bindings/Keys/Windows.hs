5ea70b9450221042630c33c8c3d423eabb838c95
Maddison Hellstrom <maddy@thundr.net>
2017-10-20 06:38:30 -0700

    Add/improve window keybindings

--- lib/Conf/Bindings/Keys/Windows.hs
+++ lib/Conf/Bindings/Keys/Windows.hs
@@ -34,28 +34,30 @@ import XMonad (sendMessage, screenWorkspace, whenJust)
 import XMonad.Actions.CopyWindow (kill1)
 import XMonad.Actions.ConditionalKeys (bindOn, XCond(LD))
 import XMonad.Actions.Navigation2D (windowGo, windowSwap, screenGo, windowToScreen, screenSwap)
+import XMonad.Layout.WindowNavigation (Direction2D(L, R))
 import XMonad.Prompt.ConfirmPrompt (confirmPrompt)
 import XMonad.Util.NamedActions (addName)
 
 windows c = subKeys "Windows" c
-  ( [ ("M-<Backspace>",   addName "Kill"            kill1)
-    , ("M-S-<Backspace>", addName "Kill all"        killAll)
-    , ("M-'",             addName "Navigate tabs D" navD)
-    , ("M-;",             addName "Navigate tabs U" navU)
-    , ("C-'",             addName "Swap tab D"      swapD)
-    , ("C-;",             addName "Swap tab U"      swapU)
-    , ("M-C-S-m",         addName "Combo swap"      comboSwap)
-
+  ( [ ("M-<Backspace>",   addName "Kill"                   kill1)
+    , ("M-S-<Backspace>", addName "Kill all"               killAll)
+    , ("M-'",             addName "Navigate tabs D"        navD)
+    , ("M-;",             addName "Navigate tabs U"        navU)
+    , ("C-'",             addName "Swap tab D"             swapD)
+    , ("C-;",             addName "Swap tab U"             swapU)
+    , ("M-S-;",           addName "Navigate window L"    $ windowGo L True)
+    , ("M-S-'",           addName "Navigate window R"    $ windowGo R True)
+    , ("M-C-S-m",         addName "Combo swap"             comboSwap)
     , ("M-<KP_End>",      addName "Select left screen"   $ selectScreen 1)
     , ("M-<KP_Down>",     addName "Select main screen"   $ selectScreen 0)
     , ("M-<KP_Insert>",   addName "Select bottom screen" $ selectScreen 2)
     ]
   ++ zipM  "M-C-"   "Merge w/sublayout"        dirKeys   dirs mergeSub
-  ++ zipM' "M-"     "Navigate window"          dirKeys   dirs windowGo       True
-  ++ zipM' "C-"     "Move window"              dirKeys   dirs windowSwap     True
-  ++ zipM' "M-S-"   "Navigate screen"          dirKeys   dirs screenGo       True
-  ++ zipM' "M-S-C-" "Move window to screen"    dirKeys   dirs windowToScreen True
-  ++ zipM' "M-"     "Swap workspace to screen" arrowKeys dirs screenSwap     True
+  ++ zipM' "M-"     "Navigate window"          dirKeys   dirs windowGo        True
+  ++ zipM' "C-S-"   "Swap window"              dirKeys   dirs windowSwap      True
+  ++ zipM' "M-S-"   "Navigate screen"          dirKeys   dirs screenGo        True
+  ++ zipM' "C-"     "Move window to screen"    dirKeys   dirs windowToScreen' True
+  ++ zipM' "M-"     "Swap workspace to screen" arrowKeys dirs screenSwap      True
   )
 
 killAll = confirmPrompt hotPrompt "kill all" $ WithAll.killAll
@@ -73,6 +75,11 @@ navU = bindOn LD
   ]
 
 selectScreen sid = screenWorkspace sid >>= flip whenJust (Operations.windows . StackSet.view)
+
+windowToScreen' d b = do
+  windowToScreen d b
+  windowGo d b
+  return ()
 
 swapD = XMonad.windows StackSet.swapDown
 swapU = XMonad.windows StackSet.swapUp
